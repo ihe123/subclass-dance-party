3ed5f835bf00d01eddfcf8d214b963de64d3c503
Hack Reactor Student <student@hackreactor.com>
2017-04-03 19:02:57 -0700

    PAssed tests, going on to implement dancers!!

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,10 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    console.log(clock.tick);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
+    
+    // console.log(clock);
   });
 
   it('should have a jQuery $node object', function() {
@@ -24,7 +27,7 @@ describe('blinkyDancer', function() {
       expect(blinkyDancer.step.callCount).to.be.equal(0);
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
-
+      // clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
